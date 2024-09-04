c4d378c825c45658f29f7996c2b6d35c6dd5f8d1
Gayathri <gayathricg4050.com>
2024-09-04 14:58:11 +0530

    colours

--- 12-package.sh
+++ 12-package.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -9,36 +8,33 @@ N="\e[0m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-      echo "please run the script with root privileges"
-      exit 1
+        echo "Please run this script with root priveleges"
+        exit 1
     fi
 }
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e $2 is ..$R "FAILED" $N
+        echo -e "$2 is...$R FAILED $N"
         exit 1
-    else 
-        echo -e $2 is ..$G "SUCCESS" $N
+    else
+        echo -e "$2 is... $G SUCCESS $N"
     fi
-
 }
 
-CHECK_ROOT()
+CHECK_ROOT
 
-for package in $@
+# sh 15-loops.sh git mysql postfix nginx
+for package in $@ # $@ refers to all arguments passed to it
 do
-  
-  dnf list installed $package
-  if [ $? -ne 0 ]
-  then 
-     echo "$package is not installed, Going to install it."
-    dnf install $package -y
-    VALIDATE $? "Installing $package"
-  else 
-     echo "$package is Installed ... Nothing to do"
-   fi
-
-
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, going to install it.."
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
+    else
+        echo "$package is already installed..nothing to do"
+    fi
 done
